884eafa8ac9287c8f628894e3a7b143ac60568c7
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2023-07-03 09:52:40 -0300

    New model sketch from scratch

--- pcr.c
+++ pcr.c
@@ -2,58 +2,49 @@
 
 Thermistor temp(1); // termistor conectado na porta A1 (cria o objeto)
 
-int RelePin1 = 2; // pino ao qual o Módulo Relé aquecimento está conectado
-int RelePin2 = 3; // pino ao qual o Módulo Relé fan está conectado
+int RelePin1 = 2; // pino ao qual o Módulo Relé fan está conectado
+int RelePin2 = 3; // pino ao qual o Módulo Relé aquecimento está conectado 
+                    //
+int limiteDisparo1 = 38;
+int limiteDisparo2 = 25;
+int tolerancia = 1;
 
-int limiteDisparo1 = 48;
-int limiteDisparo2 = 35;
-
-//float temperature = temp.getTemp(); // calcula a temperatura
 
 void setup() {
     Serial.begin(9600);
     pinMode(RelePin1, OUTPUT); // seta o pino1 como saída
     pinMode(RelePin2, OUTPUT); // seta o pino2 como saída
     digitalWrite(RelePin1, HIGH); // seta o pino com nivel logico baixo
-    digitalWrite(RelePin2, HIGH); // seta o pino com nivel logico baixo
+    digitalWrite(RelePin2, HIGH); //
 }
-
-void step01() {
-    float temperature = temp.getTemp(); // calcula a temperatura
-    if (temperature < limiteDisparo1) {
-        digitalWrite(RelePin2, LOW);
-    } else {
-        digitalWrite(RelePin2, HIGH);
-    }
-    delay(30000);
-
-    // Imprimindo no monitor serial
-    Serial.print("Temperatura: ");
-    Serial.print(temperature);
-    Serial.println("°C");
-
-    delay(1000);
-}
-
-void step02() {
-    float temperature = temp.getTemp(); // calcula a temperatura
-    if (temperature > limiteDisparo2) {
-        digitalWrite(RelePin1, LOW);
-    } else {
-        digitalWrite(RelePin1, HIGH);
-    }
-    delay(30000);
-
-    // Imprimindo no monitor serial
-    Serial.print("Temperatura: ");
-    Serial.print(temperature);
-    Serial.println("°C");
-
-    delay(1000);
-}
+ 
 
 void loop() {
-    step01();
-    step02();
-    delay(5000);
+    float temperature = temp.getTemp(); // calcula a temperatura
+    int contador = 0;
+
+    if (temperature < limiteDisparo1) {
+        digitalWrite(RelePin1, HIGH);
+        digitalWrite(RelePin2, LOW);
+        Serial.print("Aumentando a temp");
+        delay(1000);
+    } else if (temperature < (limiteDisparo1 + tolerancia ) && temperature > limiteDisparo2) {  
+        digitalWrite(RelePin2, LOW); // ligar o fan
+        digitalWrite(RelePin1, LOW); // desligar o aquecimento
+        Serial.print("Mantendo");
+        contador++;
+        Serial.print(contador);      
+        delay(1000);  
+    } else if (temperature > (limiteDisparo1 + tolerancia)) {
+        digitalWrite(RelePin2, HIGH); // ligar o fan
+        digitalWrite(RelePin1, LOW); // desligar o aquecimento
+        Serial.print("Diminuindo a temp");
+        delay (10000); 
+    }
+
+    //Imprimindo no monitor serial
+    Serial.print("Temperatura: ");
+    Serial.print(temperature);
+    Serial.println("°C");
+    delay(1000);
 }
